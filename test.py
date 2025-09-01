266e494e9f406697309b17a26bcd30923ea9c7fe
Muhammad Rizwan <48828111+rizwan78910@users.noreply.github.com>
2025-09-01 14:49:18 +0000

    Update test.py for Selenium 4

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# Get LambdaTest credentials from CI/CD variables
 LT_USERNAME = os.environ.get("LT_USERNAME")
 LT_ACCESS_KEY = os.environ.get("LT_ACCESS_KEY")
 
@@ -23,9 +22,7 @@ chrome_options = Options()
 for key, value in capabilities.items():
     chrome_options.set_capability(key, value)
 
-# Selenium 4 requires 'options' instead of 'desired_capabilities'
 driver = webdriver.Remote(command_executor=hub_url, options=chrome_options)
-
 driver.get("https://example.com")
 print("Title:", driver.title)
 driver.quit()
